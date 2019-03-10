305ad25652262c3a63c558f9006c6812468cf3c0
Stanislaviski <1769377985@qq.com>
2019-03-10 11:38:22 +0800

    2019/3/10

--- Sellonline/src/main/java/com/chris/utils/ResultVOUtil.java
+++ Sellonline/src/main/java/com/chris/utils/ResultVOUtil.java
@@ -2,10 +2,7 @@ package com.chris.utils;
 
 import com.chris.viewobj.ResultVO;
 
-/**
- * Created by 廖师兄
- * 2017-05-15 00:22
- */
+
 public class ResultVOUtil {
 
     public static ResultVO success(Object object) {
