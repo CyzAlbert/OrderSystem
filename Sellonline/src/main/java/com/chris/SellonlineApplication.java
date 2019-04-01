15aafbd1e6304457b95fb7a8c8fa7259a3a57a37
Stanislaviski <1769377985@qq.com>
2019-04-01 21:34:17 +0800

    添加数据源配置

--- Sellonline/src/main/java/com/chris/SellonlineApplication.java
+++ Sellonline/src/main/java/com/chris/SellonlineApplication.java
@@ -9,7 +9,6 @@ import org.springframework.cache.annotation.EnableCaching;
 @MapperScan(basePackages="com.chris.pojo.mapper")
 @EnableCaching
 public class SellonlineApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SellonlineApplication.class, args);
 	}
